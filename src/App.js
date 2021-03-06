2efaf6c5841b8c49a88e282337115bf8f5842319
Rangel Stoilov <rangelstoilov@gmail.com>
2021-03-06 12:37:33 +0200

    initial layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,91 @@
 import React from "react"
 import logo from "./logo.svg"
 import { Counter } from "./features/counter/Counter"
-import "bootstrap/dist/css/bootstrap.css"
 import "./App.css"
+import "bootstrap/dist/css/bootstrap.css"
 import NavBar from "./app/common/components/NavBar"
+import { Card, Button } from "react-bootstrap"
 
 function App() {
 	return (
 		<div>
 			<NavBar />
+			<div className="content">
+				<div className="vertical-split">
+					<Card className="card bg-dark text-white">
+						<Card.Header>Featured</Card.Header>
+						<Card.Body>
+							<Card.Title>Special title treatment</Card.Title>
+							<Card.Text>
+								With supporting text below as a natural lead-in to additional
+								content.
+							</Card.Text>
+							<Button variant="primary">Go somewhere</Button>
+						</Card.Body>
+					</Card>
+					<Card className="card bg-dark text-white">
+						<Card.Header>Featured</Card.Header>
+						<Card.Body>
+							<Card.Title>Special title treatment</Card.Title>
+							<Card.Text>
+								With supporting text below as a natural lead-in to additional
+								content.
+							</Card.Text>
+							<Button variant="primary">Go somewhere</Button>
+						</Card.Body>
+					</Card>
+				</div>
+				<div className="vertical">
+					<Card className="card bg-dark text-white">
+						<Card.Header>Featured</Card.Header>
+						<Card.Body>
+							<Card.Title>Special title treatment</Card.Title>
+							<Card.Text>
+								With supporting text below as a natural lead-in to additional
+								content.
+							</Card.Text>
+							<Button variant="primary">Go somewhere</Button>
+						</Card.Body>
+					</Card>
+				</div>
+				<div className="vertical-split">
+					<Card className="card bg-dark text-white">
+						<Card.Header>Featured</Card.Header>
+						<Card.Body>
+							<Card.Title>Special title treatment</Card.Title>
+							<Card.Text>
+								With supporting text below as a natural lead-in to additional
+								content.
+							</Card.Text>
+							<Button variant="primary">Go somewhere</Button>
+						</Card.Body>
+					</Card>
+					<Card className="card bg-dark text-white">
+						<Card.Header>Featured</Card.Header>
+						<Card.Body>
+							<Card.Title>Special title treatment</Card.Title>
+							<Card.Text>
+								With supporting text below as a natural lead-in to additional
+								content.
+							</Card.Text>
+							<Button variant="primary">Go somewhere</Button>
+						</Card.Body>
+					</Card>
+				</div>
+				<div className="vertical">
+					<Card className="card bg-dark text-white">
+						<Card.Header>Featured</Card.Header>
+						<Card.Body>
+							<Card.Title>Special title treatment</Card.Title>
+							<Card.Text>
+								With supporting text below as a natural lead-in to additional
+								content.
+							</Card.Text>
+							<Button variant="primary">Go somewhere</Button>
+						</Card.Body>
+					</Card>
+				</div>
+			</div>
 		</div>
 	)
 }
