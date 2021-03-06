cae51adf1ed55e23f48491cf90ab74e26ec01455
Rangel Stoilov <rangelstoilov@gmail.com>
2021-03-06 12:56:47 +0200

    add body parser and validation for orders submited

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 const express = require("express")
+const { validate, ValidationError, Joi } = require("express-validation")
+const bodyParser = require("body-parser")
 const port = process.env.PORT || 3001
 const app = express()
 const orderBook = []
+
+const orderValidation = {
+	body: Joi.object({
+		price: Joi.number().required(),
+		amount: Joi.number().required(),
+		side: Joi.string().valid("BID", "ASK").required(),
+		userId: Joi.string().required(),
+	}),
+}
+
+app.use(bodyParser.json())
 
 app.get("/getOrderbook", (req, res) => {
 	res.json(orderBook)
@@ -13,12 +26,22 @@ app.get("/getOrdersForUser/:userId", (req, res) => {
 	res.json(200)
 })
 
-app.post("/placeOrder", (req, res) => {
+app.post("/placeOrder", validate(orderValidation, {}, {}), (req, res) => {
+	const order = req.body
+	console.log(order)
 	res.json(200)
 })
 
 app.put("/cancelOrder", (req, res) => {
 	res.json(200)
+})
+
+app.use(function (err, req, res, next) {
+	if (err instanceof ValidationError) {
+		return res.status(err.statusCode).json(err)
+	}
+
+	return res.status(500).json(err)
 })
 
 app.listen(port, () =>
