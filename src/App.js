d91543a1569fadad3aa871ee103fe00bc35d10aa
Rangel Stoilov <rangelstoilov@gmail.com>
2021-03-06 17:56:23 +0200

    add place order widget

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import "./App.css"
 import "bootstrap/dist/css/bootstrap.css"
 import NavBar from "common/components/NavBar"
 import { Card, Button } from "react-bootstrap"
-import OrderBook from "features/orderBook/OrderBook"
+import OrderBook from "features/orderBook/OrderBookWidget"
 import UserWidget from "features/userWidget/UserWidget"
+import PlaceOrderWidget from "features/placeOrder/PlaceOrderWidget"
 
 function App() {
 	return (
@@ -14,17 +15,7 @@ function App() {
 			<div className="content">
 				<div className="vertical-split">
 					<UserWidget />
-					<Card className="card bg-dark text-white">
-						<Card.Header>Featured</Card.Header>
-						<Card.Body>
-							<Card.Title>Special title treatment</Card.Title>
-							<Card.Text>
-								With supporting text below as a natural lead-in to additional
-								content.
-							</Card.Text>
-							<Button variant="primary">Go somewhere</Button>
-						</Card.Body>
-					</Card>
+					<PlaceOrderWidget />
 				</div>
 				<div className="vertical">
 					<OrderBook />
