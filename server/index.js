9a9531f1868fbee287722d37cbe712173b2802b6
Rangel Stoilov <rangelstoilov@gmail.com>
2021-03-06 12:47:01 +0200

    initial endpoints

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
 const express = require("express")
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 const app = express()
+const orderBook = []
 
+app.get("/getOrderbook", (req, res) => {
+	res.json(orderBook)
+})
+
+app.get("/getOrdersForUser/:userId", (req, res) => {
+	const { userId } = req.params
+	console.log(userId)
+	res.json(200)
+})
+
+app.post("/placeOrder", (req, res) => {
+	res.json(200)
+})
+
+app.put("/cancelOrder", (req, res) => {
+	res.json(200)
+})
 
 app.listen(port, () =>
 	console.log(`REST server is listening on http://localhost:${port}/`)
