c36634ee5f7aa93643726ee1b5b905f2e6243ba2
Rangel Stoilov <rangelstoilov@gmail.com>
2021-03-06 16:46:41 +0200

    add user widget to app.js and cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react"
-import logo from "./logo.svg"
-import { Counter } from "./features/counter/Counter"
+// import logo from "./logo.svg"
 import "./App.css"
 import "bootstrap/dist/css/bootstrap.css"
-import NavBar from "./common/components/NavBar"
+import NavBar from "common/components/NavBar"
 import { Card, Button } from "react-bootstrap"
-import OrderBook from "./features/orderBook/OrderBook"
+import OrderBook from "features/orderBook/OrderBook"
+import UserWidget from "features/userWidget/UserWidget"
 
 function App() {
 	return (
@@ -13,17 +13,7 @@ function App() {
 			<NavBar />
 			<div className="content">
 				<div className="vertical-split">
-					<Card className="card bg-dark text-white">
-						<Card.Header>Featured</Card.Header>
-						<Card.Body>
-							<Card.Title>Special title treatment</Card.Title>
-							<Card.Text>
-								With supporting text below as a natural lead-in to additional
-								content.
-							</Card.Text>
-							<Button variant="primary">Go somewhere</Button>
-						</Card.Body>
-					</Card>
+					<UserWidget />
 					<Card className="card bg-dark text-white">
 						<Card.Header>Featured</Card.Header>
 						<Card.Body>
