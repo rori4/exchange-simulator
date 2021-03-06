3d50d67530ee716c0e45634124e606e0866ad471
Rangel Stoilov <rangelstoilov@gmail.com>
2021-03-06 12:26:25 +0200

    prettier all files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
-import "bootstrap/dist/css/bootstrap.css";
-import "./App.css";
-import NavBar from "./app/common/components/NavBar";
+import React from "react"
+import logo from "./logo.svg"
+import { Counter } from "./features/counter/Counter"
+import "bootstrap/dist/css/bootstrap.css"
+import "./App.css"
+import NavBar from "./app/common/components/NavBar"
 
 function App() {
-  return <NavBar />;
+	return (
+		<div>
+			<NavBar />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/app/common/components/NavBar.js
+++ src/app/common/components/NavBar.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { Navbar, Nav, Button, Form, FormControl } from "react-bootstrap";
+import React from "react"
+import { Navbar, Nav, Button, Form, FormControl } from "react-bootstrap"
 
 export default function NavBar() {
-  return (
-    <Navbar bg="primary" variant="dark">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-light">Search</Button>
-      </Form>
-    </Navbar>
-  );
+	return (
+		<Navbar bg="primary" variant="dark">
+			<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+			<Nav className="mr-auto">
+				<Nav.Link href="#home">Home</Nav.Link>
+				<Nav.Link href="#features">Features</Nav.Link>
+				<Nav.Link href="#pricing">Pricing</Nav.Link>
+			</Nav>
+			<Form inline>
+				<FormControl type="text" placeholder="Search" className="mr-sm-2" />
+				<Button variant="outline-light">Search</Button>
+			</Form>
+		</Navbar>
+	)
 }
